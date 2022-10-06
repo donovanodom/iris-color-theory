4129aa989ddc55ff31c6c45a0fa10bb1788b5e5d
div <dodomdev@gmail.com>
2022-10-06 16:53:00 +0000

    Update Color.js and styles.css

--- src/Color.js
+++ src/Color.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Color = ({h, s, l, i, hslToHex}) => {
+    const hexToRgb = function(str){
+        str = str.slice(1)
+        let RgbHex = str.match(/.{1,2}/g);
+        return `rgb(${parseInt(RgbHex[0], 16)}, ${parseInt(RgbHex[1], 16)}, ${parseInt(RgbHex[2], 16)})`
+    }
+    const hex = hslToHex(h, s, l).toUpperCase()
     return(
         <div
             className="color-block"
@@ -9,7 +15,10 @@ const Color = ({h, s, l, i, hslToHex}) => {
                 backgroundColor: `hsl(${h}, ${s}%, ${l}%)`,
             }}
         >
-            <div className='color-code'>{hslToHex(h, s, l).toUpperCase()}</div>
+            <ul className='color-code'>
+                <li>{hex}</li>
+                <li>{hexToRgb(hex)}</li>
+            </ul>
         </div>
     )
 }
