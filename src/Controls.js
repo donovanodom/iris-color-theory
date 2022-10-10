1d912974a81370650f357cb85dce74200105600f
div <dodomdev@gmail.com>
2022-10-10 00:16:38 +0000

    Update Controls.js and styles.css

--- src/Controls.js
+++ src/Controls.js
@@ -10,17 +10,17 @@ const Controls = ({generate, handleScheme, scheme}) => {
   }
   return(
     <div className="controls">
-      <div className='tools'>
+      <div className={drop ? 'a-tools' : 'tools'}>
         <div  onClick={generate} id='generate'><FaRedoAlt /></div>
         <div name="color scheme" id="color-scheme" onClick={handleDrop}>{scheme}</div>
       </div>
       <div className={drop ? 'open' : 'closed'} id='menu' onClick={handleDrop}>
-        <button onClick={handleScheme}value='Monochromatic'>MonoChromatic</button>
-        <button onClick={handleScheme}value='Analogous'>Analogous</button>
-        <button onClick={handleScheme}value='Complementary'>Complementary</button>
-        <button onClick={handleScheme}value='Split Complementary'>Split Complementary</button>
-        <button onClick={handleScheme}value='Triadic'>Triadic</button>
-        <button onClick={handleScheme}value='Square'>Square</button>
+        <button onClick={handleScheme} value='Monochromatic'>MonoChromatic</button>
+        <button onClick={handleScheme} value='Analogous'>Analogous</button>
+        <button onClick={handleScheme} value='Complementary'>Complementary</button>
+        <button onClick={handleScheme} value='Split Complementary'>Split Complementary</button>
+        <button onClick={handleScheme} value='Triadic'>Triadic</button>
+        <button onClick={handleScheme} value='Square'>Square</button>
       </div>
     </div>
   )
